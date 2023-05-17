218401aef32eb7ca3bcf07182259767a6918d74f
Nicki Shaw <33631088+NickiShaw@users.noreply.github.com>
2023-05-17 15:27:24 -0400

    Add functions to load single .emd files with single frames.

--- src/napari_emd/_reader.py
+++ src/napari_emd/_reader.py
@@ -6,6 +6,101 @@ implement multiple readers or even other plugin contributions. see:
 https://napari.org/stable/plugins/guides.html?#readers
 """
 import numpy as np
+import h5py
+from matplotlib import pyplot as plt
+
+class navigate:
+
+    @staticmethod
+    def getGroupsNames(group):
+        items = []
+        for item in group:
+            if group.get(item, getclass=True) == h5py._hl.group.Group:
+                items.append(group.get(item).name)
+        print(items)
+
+    @staticmethod
+    def getGroup(group, item):
+        if group.get(item, getclass=True) == h5py._hl.group.Group:
+            return group.get(item)
+
+    @staticmethod
+    def getSubGroup(group, path):
+        return group[path]
+
+    @staticmethod
+    def getDirectoryMap(group):
+        for item in group:
+            # check if group
+            if group.get(item, getclass=True) == h5py._hl.group.Group:
+                item = group.get(item)
+                # check if emd_group_type
+                # if 'emd_group_type' in item.attrs:
+                print('found a group emd at: {}'.format(item.name))
+                # process subgroups
+                if type(item) is h5py._hl.group.Group:
+                    navigate.getDirectoryMap(item)
+                else:
+                    print('found an emd at: {}'.format(item))
+                    # print(type(item))
+
+    @staticmethod
+    def getMemberName(group, path):
+        members = list(group[path].keys())
+        if len(members) == 1:
+            return str(members[0])
+        else:
+            return members
+
+    @staticmethod
+    def parseFileName(file):
+        return str(file).split("/")[-1].split(".")[0]
+
+class EMDreader:
+    """
+    Unpacks emd data files with the h5py package, by navigating subdirectories.
+
+    Parameters
+    ----------
+    path : str
+        Path to file, NOT a list of paths.
+    """
+    def __init__(self, singlePath: str):
+        self.singleH5pyObject = h5py.File(singlePath, 'r')
+
+    def unpackData(self):
+
+        # TODO add implementation to search subfolders if the format is not the Velox default.
+
+        try:
+            data = self.singleH5pyObject['Data/Image/' + navigate.getMemberName(self.singleH5pyObject, '/Data/Image/') + '/Data']
+            return np.array(data)
+
+        except:
+            raise ValueError("File was not able to be read, see unpackData function.")
+    def parseEMDdata(self):
+        """
+        Returns
+        -------
+        tuple
+            Returns a tuple with the contents: (data, add_kwargs, layer_type).
+            data : image data.
+            add_kwargs : metadata for each frame.
+            layer_type : "image".
+        """
+
+        data = self.unpackData()
+        data = data.reshape(data.shape[0], data.shape[1])
+        print(data.shape)
+
+        add_kwargs = {}
+
+        layer_type = "image"  # optional, default is "image"
+
+        return (data, add_kwargs, layer_type)
+
+
+
 
 
 def napari_get_reader(path):
@@ -22,11 +117,6 @@ def napari_get_reader(path):
         If the path is a recognized format, return a function that accepts the
         same path or list of paths, and returns a list of layer data tuples.
     """
-    if isinstance(path, list):
-        # reader plugins may be handed single path, or a list of paths.
-        # if it is a list, it is assumed to be an image stack...
-        # so we are only going to look at the first file.
-        path = path[0]
 
     # if we know we cannot read the file, we immediately return None.
     if not path.endswith(".emd"):
@@ -58,15 +148,63 @@ def reader_function(path):
         layer. Both "meta", and "layer_type" are optional. napari will
         default to layer_type=="image" if not provided
     """
-    # handle both a string and a list of strings
-    paths = [path] if isinstance(path, str) else path
-    # load all files into array
-    arrays = [np.load(_path) for _path in paths]
-    # stack arrays into single array
-    data = np.squeeze(np.stack(arrays))
 
-    # optional kwargs for the corresponding viewer.add_* method
-    add_kwargs = {}
+    # Assume that a list of strings are all separate files that wish to be opened.
+    if isinstance(path, list):
+        # Return a list of LayerData tuples if multiple files are opened.
+        LayerDataList = []
+        for file in path:
+            LayerDataList.append(EMDreader(file).parseEMDdata())
+        return LayerDataList
+    else:
+        # Return a single LayerData tuple.
+        return [EMDreader(path).parseEMDdata()]
 
-    layer_type = "image"  # optional, default is "image"
-    return [(data, add_kwargs, layer_type)]
+
+
+    # # handle both a string and a list of strings
+    # paths = [path] if isinstance(path, str) else path
+    # # load all files into array
+    # arrays = [np.load(_path) for _path in paths]
+    # # stack arrays into single array
+    # data = np.squeeze(np.stack(arrays))
+    #
+    # # optional kwargs for the corresponding viewer.add_* method
+    # add_kwargs = {"metadata": ['test metadata']}
+    #
+    # layer_type = "image"  # optional, default is "image"
+    # return [(data, add_kwargs, layer_type)]
+
+
+from magicgui import magicgui
+import napari
+from napari.types import ImageData, LayerDataTuple
+from typing import List
+import numpy as np
+
+
+# def run_segment(*args, **kwargs):
+#     print('running segment')
+#     raise ValueError("I don't actually work")
+#     return [(args[0], {}, 'image')]
+#
+#
+# @magicgui(call_button="Segment")
+# def segmentation(
+#     data: ImageData, masks: ImageData, background: bool, membrane_width: int, min_size: int
+# ) -> List[LayerDataTuple]:
+#     return run_segment(data, masks, background, membrane_width, min_size)
+
+
+# print(image[0].shape)
+#
+# image1 = np.random.random((512, 512))
+# print(image1.shape)
+# stack = np.squeeze(np.stack([image1,image1,image1,image1]))
+
+
+# viewer.window.add_dock_widget(segmentation)
+
+#image = EMDreader(r"C:\Users\shawn\Downloads\feature.emd").parseEMDdata()
+#viewer = napari.view_image(image[0])
+#napari.run()
