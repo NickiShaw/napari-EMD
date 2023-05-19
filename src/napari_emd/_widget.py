754e56df7ee63b962755d572d4140cc3ac2f2372
Nicki Shaw <33631088+NickiShaw@users.noreply.github.com>
2023-05-19 16:25:41 -0400

    Added scrollbar update (prints multiples)

--- src/napari_emd/_widget.py
+++ src/napari_emd/_widget.py
@@ -15,6 +15,7 @@ from qtpy.QtWidgets import QHBoxLayout, QPushButton, QWidget, QLabel
 if TYPE_CHECKING:
     import napari
 
+from napari.utils.events import Event
 
 class EMDWidget(QWidget):
     def __init__(self, napari_viewer):
@@ -24,11 +25,25 @@ class EMDWidget(QWidget):
         btn = QPushButton("Click me!")
         btn.clicked.connect(self._on_click)
 
-        lb = QLabel("some text")
-
         self.setLayout(QHBoxLayout())
         self.layout().addWidget(btn)
+
+
+        self.viewer.dims.events.current_step.connect(self.update_metadata)
+
+        # def print_layer_name(event):
+        #     print(f"{event.source.name} changed its data!")
+        #
+        # self.viewer.layers
+        # layer.events.data.connect(print_layer_name)
+
+        # lb = QLabel('frame is' + str(self.viewer.dims.current_step))
+        # self.layout().addWidget(lb)
+
+    def update_metadata(self, metadataWidget):
+        lb = QLabel('frame is' + str(self.viewer.dims.current_step[0]))
         self.layout().addWidget(lb)
+        print('frame is' + str(self.viewer.dims.current_step[0]))
 
     def _on_click(self):
         print("napari has", len(self.viewer.layers), "layers")
